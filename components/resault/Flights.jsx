de59e2d59d694fae7a3cd17c88457a8b92d4b85e
mahdi <mmiq69@gmail.com>
2024-06-10 13:02:25 -0400

    'dynamo'

--- components/resault/Flights.jsx
+++ components/resault/Flights.jsx
@@ -3,28 +3,41 @@ import React, { useState, useEffect } from "react";
 import Api from "../../middleware/Api";
 import CustomButton from "../CustomButton";
 import { db } from "../../middleware/FireBaseConfig";
-import {useGlobalContext} from '../../context/GlobalProvider'
-import {
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  deleteDoc,
-} from "firebase/firestore";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { collection, addDoc } from "firebase/firestore";
+import AwsGateway from "../../middleware/AwsGateway";
 const Flights = ({ formData }) => {
   const [flightData, setFlightData] = useState([]);
-  const {user} = useGlobalContext()
-  const userId = user.id
+  const { user } = useGlobalContext();
+  const [originIndex, setOriginIndex] = useState('')
+  const [desteinationIndex, setDestenationIndex] = useState('')
+  const userId = user.id;
   useEffect(() => {
-    const { origin, destenation, departureDate, passengerNumber } = formData;
+    const { origin, destenation } = formData;
+    const findAirports = async (location, setter) => {
+      try {
+        const res = await AwsGateway(location);
+        if (res.ok) {
+          setter(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (formData) {
+      findAirports(origin, setOriginIndex)
+      findAirports(destenation, setDestenationIndex);
+    }
+  }, [formData]);
+  useEffect(() => {
+    const { departureDate, passengerNumber } = formData;
     const flightFunc = async () => {
       try {
         const res = Api.flightApi({
-          origin,
-          destenation,
-          departureDate,
-          passengerNumber,
+          origin: originIndex,
+          destenation: desteinationIndex,
+          departureDate: departureDate,
+          passengerNumber: passengerNumber
         });
         if (res.ok) {
           const data = await res.json();
@@ -35,20 +48,20 @@ const Flights = ({ formData }) => {
       }
     };
     flightFunc();
-  }, [formData]);
+  }, [formData, originIndex, desteinationIndex]);
   const handleSubmit = async (plane) => {
     try {
-      if(!user){
-        console.log('no user found')
-        return 
+      if (!user) {
+        console.log("no user found");
+        return;
       }
       const markOj = {
         origin: plane.origin,
         destenation: plane.destenation,
         departureDate: plane.departureDate,
         pirce: plane.price,
-        userId: userId
-      }
+        userId: userId,
+      };
       await addDoc(collection(db, "flightBookMark"), { markOj });
       console.log("added successfully");
     } catch (error) {
